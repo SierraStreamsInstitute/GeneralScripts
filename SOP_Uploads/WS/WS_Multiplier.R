09fc60392e601384b55663f077b6bb3e8a7dfc39
westonslaughter <63979914+westonslaughter@users.noreply.github.com>
2020-09-14 15:19:22 -0400

    Tutorial push

---
+++ SOP_Uploads/WS/WS_Multiplier.R
@@ -0,0 +1,13 @@
+#### Kyle Example Script
+#### Sierra Streams Institute
+#### Author: Wes Slaughter
+#### Date: 9/14/2020
+
+#### Below is a function that will take any input and multiply it
+#### by 420
+
+kyleMultiply <- function(x){
+  x * 420
+}
+
+kyleMultiply(42)
